dbdad5c0bffdfff376100817c62f8e56d8737876
Jay D <jduffy@chicagobooth.edu>
2016-10-31 15:33:30 -0500

    jjduffy333_photogram_71percent

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -21,5 +21,21 @@ class PhotosController < ApplicationController
       redirect_to("http://localhost:3000/photos")
     end
 
+    def delete_row
+      d = Photo.find_by({ :id => params[:id]})
+      d.destroy
+      d.save
+
+      # render("create_row.html.erb")
+      redirect_to("http://localhost:3000/photos")
+
+    end
+
+    def edit_row
+      e = Photo.find_by({ :id => params[:id]})
+      e.caption = params[:the_caption_new]
+      e.source = params[:the_source_new]
+      e.save
+    end
+
 end
- 
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,17 @@ Rails.application.routes.draw do
 
   get("/", { :controller => "photos", :action => "index" })
 
-  #routs to CREATE photos
+  #routes to CREATE photos
   get("/photos/new", { :controller => "photos", :action => "new_form"})
   get("/create_photo", { :controller => "photos", :action => "create_row"})
+
+  #routes to EDIT photos
+
+  get("/photos/:id/edit", { :controller => "photos", :action => "edit_row"})  
+
+  #routes to DELETE photos
+
+  get("/delete_photo/:id", { :controller => "photos", :action => "delete_row"})
 
   # Routes to READ photos
   get("/photos",           { :controller => "photos", :action => "index" })
